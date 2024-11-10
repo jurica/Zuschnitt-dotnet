7ef999ec904054b3d58029294d25ad71da3b276d
Jurica Bacurin <jurica@bacurin.de>
2024-11-10 21:28:24 +0100

    wip

--- Zuschnitt.Models/Project.cs
+++ Zuschnitt.Models/Project.cs
@@ -1,27 +1,61 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Zuschnitt.Models;
-public class Project 
+
+public class Project
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public string Name { get; set; } = "";
     public Sheet? SelectedSheet { get; set; }
-    [JsonIgnore] public IEnumerable<Sheet> Sheets
+
+    [JsonIgnore]
+    public IEnumerable<Sheet> Sheets
     {
         get
         {
-           foreach (var sheet in _sheets) yield return sheet;
+            foreach (var sheet in _sheets) yield return sheet;
         }
     }
+
     [JsonInclude] internal List<Sheet> _sheets { get; init; } = new();
-    
+
+    [JsonIgnore] private static JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        ReferenceHandler = ReferenceHandler.Preserve,
+        WriteIndented = true
+    };
+
     public void AddSheet()
     {
-        new Sheet(){Parent = this};
+        new Sheet()
+        {
+            Parent = this
+        };
     }
 
-    public void RemoveSheet(Sheet sheet)
+    public void DeleteSheet(Sheet sheet)
     {
         _sheets.Remove(sheet);
+        if (!_sheets.Any())
+        {
+            AddSheet();
+        }
+        else
+        {
+            SelectedSheet = _sheets.First();
+        }
+    }
+
+    public static Project? Deserialize(Stream stream)
+    {
+        var p = JsonSerializer.Deserialize<Project>(stream, _jsonSerializerOptions);
+
+        return p;
+    }
+
+    public byte[] Serialize()
+    {
+        return JsonSerializer.SerializeToUtf8Bytes(this, _jsonSerializerOptions);
     }
 }
--- Zuschnitt.Models/Sheet.cs
+++ Zuschnitt.Models/Sheet.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.Text.Json.Serialization;
 
 namespace Zuschnitt.Models;
@@ -60,5 +59,10 @@ public class Sheet
     public int ColumnPos(Column column)
     {
         return _columns.IndexOf(column);
+    }
+
+    public void Delete()
+    {
+        _parent.DeleteSheet(this);
     }
 }
